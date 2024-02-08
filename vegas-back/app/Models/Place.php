8b255ffcd903aca1e7038a4eb9ed19437a0156c3
LMan <a.hamel4480@gmail.com>
2024-02-08 10:41:43 +0100

    Lien User  Place

--- vegas-back/app/Models/Place.php
+++ vegas-back/app/Models/Place.php
@@ -19,11 +19,17 @@ class Place extends Model
         'description',
         'extras',
         'picture',
-        'category_id'
+        'category_id',
+        'user_id'
     ];
 
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
