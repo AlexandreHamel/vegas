2d25d9db2bcc748fad55de613eba17aa465a5a0f
LMan <a.hamel4480@gmail.com>
2024-02-19 16:07:58 +0100

    Map Detail Debug

--- vegas-front/src/components/LeafletMap.js
+++ vegas-front/src/components/LeafletMap.js
@@ -2,29 +2,20 @@ import React from 'react';
 import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const LeafletMap = ( { place } ) => {
-
-    const lat = place.latitude;
-    const long = place.longitude
-
-    const position = [36.11589001, -115.17322117]
-
-    console.log(lat);
-    console.log(long);
-
+const LeafletMap = ({ place }) => {
 
     return (
         <div id='map'>
-            <MapContainer 
-                style={{height: '100%', width: '100%'}}
-                center={ position } 
-                zoom={16} 
+            <MapContainer
+                style={{ height: '100%', width: '100%' }}
+                center={[place.latitude, place.longitude]}
+                zoom={16}
                 scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={ position }>
+                <Marker position={[place.latitude, place.longitude]}>
                     <Popup>
                         {place.namePlace} <br /> {place.address}
                     </Popup>
--- vegas-front/src/components/place/placeDetails.js
+++ vegas-front/src/components/place/placeDetails.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import Detail from '../Detail';
+import LeafletMap from '../LeafletMap';
 
 const PlaceDetails = () => {
 
     const { id } = useParams();
     const [place, setPlace] = useState({});
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         axios
@@ -17,18 +18,48 @@ const PlaceDetails = () => {
             )
             .then((response) => {
                 setPlace(response.data);
+                setIsLoaded(true);
 
-                console.log(response.data);
+                console.log(response.data.category.nameCategory);
 
             })
             .catch(error => {
                 console.error('Erreur', error);
             })
-    }, [id]);
+    }, [id, isLoaded]);
 
     return (
         <>
-            <Detail place={place} />
+            <div className='container'>
+                <div className="detail">
+                    <img src={`http://127.0.0.1:8000/storage/uploads/${place.picture}`} alt={place.picture} />
+                    <div className="detail-side">
+                        {isLoaded === true ? (
+                            <span> {place.category.nameCategory} </span>
+                        ) : ("")}
+                        <h1> {place.namePlace && place.namePlace.toUpperCase()} </h1>
+                        <p> {place.description}</p>
+
+                        <p> {place.extras} </p>
+                        <address>
+                            <h4>Adresse : </h4>
+                            <p> {place.address}</p>
+                        </address>
+                        <h4>Coordonnées GPS: </h4>
+                        <p>{" "}{place.latitude}, {place.longitude}{" "}</p>
+                    </div>
+                </div>
+                <div className="detail-map">
+                    <h2>Localisation :</h2>
+
+                    <div id='map'>
+                        {isLoaded === true ? (
+                            <LeafletMap place={place} />
+                        ) : ("")}
+                    </div>
+
+                </div>
+            </div>
         </>
     );
 };
